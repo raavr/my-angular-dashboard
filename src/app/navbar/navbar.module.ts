e0882003548531216aec2f8b97fd11f005b6563d
raavr <rafal.r26@gmail.com>
2017-08-15 19:53:12 +0200

    add router link to navbar and modify styles

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [ CommonModule ],
+    imports: [ 
+        CommonModule, 
+        RouterModule 
+    ],
     declarations: [
         NavbarComponent
     ],

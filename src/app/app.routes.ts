aed5a5339066d37b012d032a95c55c4cebf4b00b
raavr <rafal.r26@gmail.com>
2017-08-17 21:22:49 +0200

    add default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router'
 import { NoContentComponent } from './no-content';
 
 export const ROUTES: Routes = [
+  { path: '', redirectTo: "/resources",  pathMatch: 'full' },
   { path: '**',    component: NoContentComponent }
 ];
